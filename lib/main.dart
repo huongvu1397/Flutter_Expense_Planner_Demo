1a966ed296d4a7e5621d419c85fcee66809d3591
huongvu <huongvu1397@gmail.com>
2022-06-22 02:41:34 +0700

    Change UserTransaction simple UI to simple BottomSheet Update MyHomePage to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_expense_planner_demo/widgets/user_transaction.dart';
+import 'package:flutter_expense_planner_demo/widgets/new_transaction.dart';
+import '/widgets/transaction_list.dart';
+
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,15 +18,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  //String? titleInput;
-  //String? amountInput;
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', title: 'New Shoes', ammount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        ammount: 16.53,
+        date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        ammount: txAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: (){},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Planner XXX'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context))
+        ],
       ),
       body: Column(
         //mainAxisAlignment: MainAxisAlignment.start,
@@ -41,8 +85,13 @@ class MyHomePage extends StatelessWidget {
             color: Colors.red,
             child: Text('LIST OF TX'),
           ),
-          const UserTransaction()
+          TransactionList(_userTransaction),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
